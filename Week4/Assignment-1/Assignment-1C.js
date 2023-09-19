7fbd2d780cc4ae9d691151f879b8fa6a48bd527e
HTWu666 <huitingwuuu@gmail.com>
2023-09-19 22:55:20 +0800

    style: add semicolon to the end of the statement

--- Week4/Assignment-1/Assignment-1C.js
+++ Week4/Assignment-1/Assignment-1C.js
@@ -9,7 +9,7 @@ function delayedResultPromise(n1, n2, delayTime) {
 
 async function main() {
     // your code here, you should call delayedResultPromise here and get the result using async/await.
-    const result = await delayedResultPromise(4, 5, 3000)
+    const result = await delayedResultPromise(4, 5, 3000);
     console.log(result);
 }
 
