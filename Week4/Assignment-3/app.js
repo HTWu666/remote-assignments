6e2361c458ba006c69757163859a66e10e8afd41
HTWu666 <huitingwuuu@gmail.com>
2023-09-20 16:24:47 +0800

    refactor: Separate the MySQL operation from app.js as database.js

--- Week4/Assignment-3/app.js
+++ Week4/Assignment-3/app.js
@@ -1,8 +1,6 @@
 const express = require('express')
-const mysql = require('mysql2/promise')
 const port = 3000;
-const dotenv = require('dotenv')
-dotenv.config()
+const { signUp, signIn } = require('./database.js')
 
 const app = express()
 
@@ -10,47 +8,18 @@ const app = express()
 app.use(express.static('./public'))
 app.use(express.json())
 
-// DB connection pool
-const pool = mysql.createPool({
-    host: process.env.MYSQL_HOST,
-    user: process.env.MYSQL_USER,
-    password: process.env.MYSQL_PASSWORD,
-    database: process.env.MYSQL_DATABASE
-})
-
-const getConnection = () => pool.getConnection()
-const query = (sql, values) => pool.query(sql, values)
-
 // signup endpoint
 app.post('/signup', async(req, res) => {
     const { email, password } = req.body
-
-    const connection = await getConnection()
-    const [rows] = await query('SELECT * FROM user WHERE email = ?', [email])
-
-    if (!rows.length) {
-        await query('INSERT INTO user (email, password) VALUES (?, ?)', [email, password])
-        connection.release()
-        return res.json({success: true})
-    } else {
-        connection.release()
-        return res.json({success: false})
-    }
+    const result = await signUp(email, password)
+    res.json(result)
 })
 
 // signin endpoint
 app.post('/signin', async(req, res) => {
     const { email, password } = req.body
-
-    const connection = await getConnection()
-    const [rows] = await query('SELECT * FROM user WHERE email = ? AND password = ?', [email, password])
-    connection.release()
-
-    if (!rows.length) {
-        return res.json({ success: false, msg: 'email or password is wrong!' });
-    } else {
-        return res.json({ success: true });
-    }
+    const result = await signIn(email, password)
+    res.json(result)
 })
 
 // run the server
