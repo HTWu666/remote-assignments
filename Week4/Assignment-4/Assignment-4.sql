5ef33824d41f4d6f679965b20fb6c09d35c13164
HTWu666 <huitingwuuu@gmail.com>
2023-09-22 01:01:26 +0800

    fix: Fix an instruction error

--- Week4/Assignment-4/Assignment-4.sql
+++ Week4/Assignment-4/Assignment-4.sql
@@ -1,5 +1,5 @@
 -- Write an SQL statement to select all articles with their author’s email.
-SELECT email, title, content FROM user INNER JOIN article ON user.id=article.user_id WHERE user.email='test';
+SELECT email, title, content FROM user INNER JOIN article ON user.id=article.user_id;
 
 -- Write another SQL statement to select articles from 7th to 12th sorted by id.
 SELECT * FROM article ORDER BY id LIMIT 6 OFFSET 6;
