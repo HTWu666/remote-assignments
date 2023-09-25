1fbcc49f81a44a0e8b303f50fef12510138a8b8c
HTWu666 <huitingwuuu@gmail.com>
2023-09-25 11:16:05 +0800

    refactor: Use INNER JOIN and LEFT JOIN instead of RIGHT JOIN

--- Week4/Assignment-4/Assignment-4.sql
+++ Week4/Assignment-4/Assignment-4.sql
@@ -1,5 +1,6 @@
 -- Write an SQL statement to select all articles with their author’s email.
-SELECT user.email, article.title, article.content FROM user RIGHT JOIN article ON user.id=article.user_id;
+SELECT user.email, article.title, article.content FROM article LEFT JOIN user ON user.id=article.user_id;
+SELECT user.email, article.title, article.content FROM article INNER JOIN user ON user.id=article.user_id;
 
 -- Write another SQL statement to select articles from 7th to 12th sorted by id.
 SELECT * FROM article ORDER BY id LIMIT 6 OFFSET 6;
